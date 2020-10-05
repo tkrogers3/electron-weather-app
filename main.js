9765d4e1abbfacf0b09dbd8c92b81f84afe955e5
tkrogers3 <tkrogers3@gmail.com>
2020-10-05 12:25:16 -0400

    whenReady error

--- main.js
+++ main.js
@@ -22,62 +22,67 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow)
+
+/*Uncaught TypeError: Cannot read property 'whenReady' of undefined
+at main.js:24 */
+
+
 //setting variables and their ID handlers
-var api = "https://api.openweathermap.org/data/2.5/weather?zip=";
-var apiKey = ",&appid=85fbdf671b1a59edaae1f9e998fdcbad";
-var btn = document.getElementById("btnSearch");
-var cityName = document.getElementById("cityName");
-var k = document.getElementById("kelvin");
-var f = document.getElementById("fahrenheit");
-var c = document.getElementById("celsius");
-var wthCon = document.getElementById("con");
-var iconImg = document.getElementById("icon");
-var zipCode = document.getElementById("zipInfo");
-var alert = document.getElementById("alert");
-var cards = document.getElementById("card-deck");
-document.getElementById('btnSearch').addEventListener('click', getWeather);
+// var api = "https://api.openweathermap.org/data/2.5/weather?zip=";
+// var apiKey = ",&appid=85fbdf671b1a59edaae1f9e998fdcbad";
+// var btn = document.getElementById("btnSearch");
+// var cityName = document.getElementById("cityName");
+// var k = document.getElementById("kelvin");
+// var f = document.getElementById("fahrenheit");
+// var c = document.getElementById("celsius");
+// var wthCon = document.getElementById("con");
+// var iconImg = document.getElementById("icon");
+// var zipCode = document.getElementById("zipInfo");
+// var alert = document.getElementById("alert");
+// var cards = document.getElementById("card-deck");
+// document.getElementById('btnSearch').addEventListener('click', getWeather);
 
-/* Preventative coding - Not allowing letters or  "+"" | "-" |"e" characters as a part of 
-this statement.*/
-var z = zipCode.length;
-if (z == 5 && (!isNaN(z)));
+// /* Preventative coding - Not allowing letters or  "+"" | "-" |"e" characters as a part of 
+// this statement.*/
+// var z = zipCode.length;
+// if (z == 5 && (!isNaN(z)));
 
-/*Function to take zip code info from user and pull info from 
-JSON data received from API Call. If valid, get weather info.
- If invalid, get error message. */
-function getWeather() {
-    fetch(api + zipCode.value + apiKey)    // Fetch = Promise
-        .then((response) => {
-            if (!response.ok) {
-                throw response
-            } return response.json();
+// /*Function to take zip code info from user and pull info from 
+// JSON data received from API Call. If valid, get weather info.
+//  If invalid, get error message. */
+// function getWeather() {
+//     fetch(api + zipCode.value + apiKey)    // Fetch = Promise
+//         .then((response) => {
+//             if (!response.ok) {
+//                 throw response
+//             } return response.json();
 
-        })
-        /*once promise has been met, then data is processed and presented to screen.
-        k and f strings temperate literal to embed expression. Math.floor to round 
-        down to closest integer.*/
+//         })
+//         /*once promise has been met, then data is processed and presented to screen.
+//         k and f strings temperate literal to embed expression. Math.floor to round 
+//         down to closest integer.*/
 
-        .then((data) => {   //Promise fulfilled 
+//         .then((data) => {   //Promise fulfilled 
 
-            //var info = data;
-            cityName.innerHTML = data.name;
-            k.innerHTML = `${Math.floor(data.main.temp)}<sup>o</sup>K `;
-            f.innerHTML = `${Math.floor(((data.main.temp - 273.15) * 9 / 5 + 32))}<sup>o</sup>F`;
-            c.innerHTML = `${Math.floor(data.main.temp - 273.15)}<sup>o</sup>C`;
-            wthCon.innerHTML = data.weather[0].main;
+//             //var info = data;
+//             cityName.innerHTML = data.name;
+//             k.innerHTML = `${Math.floor(data.main.temp)}<sup>o</sup>K `;
+//             f.innerHTML = `${Math.floor(((data.main.temp - 273.15) * 9 / 5 + 32))}<sup>o</sup>F`;
+//             c.innerHTML = `${Math.floor(data.main.temp - 273.15)}<sup>o</sup>C`;
+//             wthCon.innerHTML = data.weather[0].main;
 
-            //Template Literal that concatenates the link with the icon to display image  into png.
+//             //Template Literal that concatenates the link with the icon to display image  into png.
 
-            iconImg.innerHTML = `<img src ="https://openweathermap.org/img/wn/${info.weather[0].icon}.png">`
-            alert.className = "d-none alert";
-            info.weather[0].icon;
-            })
+//             iconImg.innerHTML = `<img src ="https://openweathermap.org/img/wn/${info.weather[0].icon}.png">`
+//             alert.className = "d-none alert";
+//             info.weather[0].icon;
+//             })
         
-        .catch((error) => { //Promise rejected 
-            //If error has occurred, alert displays below zip search bar.
-            alert.className = "d-block alert-warning ";
-        })
-}
+//         .catch((error) => { //Promise rejected 
+//             //If error has occurred, alert displays below zip search bar.
+//             alert.className = "d-block alert-warning ";
+//         })
+// }
 
 
 // Quit when all windows are closed, except on macOS. There, it's common
